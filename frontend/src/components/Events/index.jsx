201d28ab43a50ae13e6860d27cb41db50b85982c
breeku <bregfi@hotmail.com>
2020-09-16 13:55:56 +0300

    hakee kaikki tagit

--- frontend/src/components/Events/index.jsx
+++ frontend/src/components/Events/index.jsx
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react'
 
 import { makeStyles } from '@material-ui/core/styles'
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-import Button from '@material-ui/core/Button';
+import InputLabel from '@material-ui/core/InputLabel'
+import MenuItem from '@material-ui/core/MenuItem'
+import FormControl from '@material-ui/core/FormControl'
+import Select from '@material-ui/core/Select'
+import Button from '@material-ui/core/Button'
 
 import { getEvents } from '../../services/events'
-import { getTagNames } from '../../services/getTagNames';
+import { getTagNames } from '../../services/getTagNames'
 
 const useStyles = makeStyles(theme => ({
     text_center: theme.text_center,
@@ -34,49 +34,49 @@ const useStyles = makeStyles(theme => ({
 export default function Events() {
     const [events, setEvents] = useState(null)
     const classes = useStyles()
-    const [language, setLanguage] = React.useState(null);
-    const [tags, setTags] = React.useState('music');
-    const [tagList, setTagList] = React.useState([]);
-    const [openLang, setOpenLang] = React.useState(false);
-    const [openResult, setOpenResult] = React.useState(false);
-    const [openTags, setOpenTags] = React.useState(false);
-    const [resultLimit, setResultLimit] = React.useState(10);
+    const [language, setLanguage] = React.useState(null)
+    const [tags, setTags] = React.useState('music')
+    const [tagList, setTagList] = React.useState([])
+    const [openLang, setOpenLang] = React.useState(false)
+    const [openResult, setOpenResult] = React.useState(false)
+    const [openTags, setOpenTags] = React.useState(false)
+    const [resultLimit, setResultLimit] = React.useState(10)
 
-    const handleChangeTags = (event) => {
-        setTags(event.target.value);
-    };
+    const handleChangeTags = event => {
+        setTags(event.target.value)
+    }
 
     const handleCloseTags = () => {
-        setOpenTags(false);
-    };
+        setOpenTags(false)
+    }
 
     const handleOpenTags = () => {
-        setOpenTags(true);
-    };
+        setOpenTags(true)
+    }
 
-    const handleChangeLang = (event) => {
-        setLanguage(event.target.value);
-    };
+    const handleChangeLang = event => {
+        setLanguage(event.target.value)
+    }
 
     const handleCloseLang = () => {
-        setOpenLang(false);
-    };
+        setOpenLang(false)
+    }
 
     const handleOpenLang = () => {
-        setOpenLang(true);
-    };
+        setOpenLang(true)
+    }
 
-    const handleChangeResultLimit = (event) => {
-        setResultLimit(event.target.value);
-    };
-    
+    const handleChangeResultLimit = event => {
+        setResultLimit(event.target.value)
+    }
+
     const handleCloseResultLimit = () => {
-        setOpenResult(false);
-    };
-    
+        setOpenResult(false)
+    }
+
     const handleOpenResultLimit = () => {
-        setOpenResult(true);
-    };
+        setOpenResult(true)
+    }
 
     useEffect(() => {
         const getData = async () => {
@@ -89,13 +89,12 @@ export default function Events() {
 
     useEffect(() => {
         const getData = async () => {
-            const response = await getTagNames(tagNames)
+            const response = await getTagNames()
             setTagList(response.data)
         }
-
-        console.log(response)
-    }, [tagNames])
-
+        if (tagList.length === 0) getData()
+    }, [])
+    console.log(tagList)
     return (
         <>
             <div>
@@ -105,18 +104,17 @@ export default function Events() {
                 <FormControl className={classes.formControl}>
                     <InputLabel id="demo-controlled-open-select-label">Tag</InputLabel>
                     <Select
-                    labelId="demo-controlled-open-select-label"
-                    id="demo-controlled-open-select"
-                    open={openTags}
-                    onClose={handleCloseTags}
-                    onOpen={handleOpenTags}
-                    value={tags}
-                    onChange={handleChangeTags}
-                    >
-                    <MenuItem value={'music'}>Music</MenuItem>
-                    <MenuItem value={'General'}>General</MenuItem>
-                    <MenuItem value={'Pupils'}>Pupils</MenuItem>
-                    <MenuItem value={'Libraries'}>Libraries</MenuItem>
+                        labelId="demo-controlled-open-select-label"
+                        id="demo-controlled-open-select"
+                        open={openTags}
+                        onClose={handleCloseTags}
+                        onOpen={handleOpenTags}
+                        value={tags}
+                        onChange={handleChangeTags}>
+                        <MenuItem value={'music'}>Music</MenuItem>
+                        <MenuItem value={'General'}>General</MenuItem>
+                        <MenuItem value={'Pupils'}>Pupils</MenuItem>
+                        <MenuItem value={'Libraries'}>Libraries</MenuItem>
                     </Select>
                 </FormControl>
             </div>
@@ -127,47 +125,48 @@ export default function Events() {
                 <FormControl className={classes.formControl}>
                     <InputLabel id="demo-controlled-open-select-label">Kieli</InputLabel>
                     <Select
-                    labelId="demo-controlled-open-select-label"
-                    id="demo-controlled-open-select"
-                    open={openLang}
-                    onClose={handleCloseLang}
-                    onOpen={handleOpenLang}
-                    value={language}
-                    onChange={handleChangeLang}
-                    >
-                    <MenuItem value="">
-                        <em>None</em>
-                    </MenuItem>
-                    <MenuItem value={'fi'}>FI</MenuItem>
-                    <MenuItem value={'en'}>EN</MenuItem>
-                    <MenuItem value={'sv'}>SV</MenuItem>
-                    <MenuItem value={'zh'}>ZH</MenuItem>
+                        labelId="demo-controlled-open-select-label"
+                        id="demo-controlled-open-select"
+                        open={openLang}
+                        onClose={handleCloseLang}
+                        onOpen={handleOpenLang}
+                        value={language}
+                        onChange={handleChangeLang}>
+                        <MenuItem value="">
+                            <em>None</em>
+                        </MenuItem>
+                        <MenuItem value={'fi'}>FI</MenuItem>
+                        <MenuItem value={'en'}>EN</MenuItem>
+                        <MenuItem value={'sv'}>SV</MenuItem>
+                        <MenuItem value={'zh'}>ZH</MenuItem>
                     </Select>
                 </FormControl>
             </div>
             <div>
-                <Button className={classes.button} id="resultLimit" onClick={handleOpenResultLimit}>
+                <Button
+                    className={classes.button}
+                    id="resultLimit"
+                    onClick={handleOpenResultLimit}>
                     Valitse hakutulosten määrä
                 </Button>
                 <FormControl className={classes.formControl}>
                     <InputLabel id="demo-controlled-open-select-label">Määrä</InputLabel>
                     <Select
-                    labelId="demo-controlled-open-select-label"
-                    id="demo-controlled-open-select"
-                    open={openResult}
-                    onClose={handleCloseResultLimit}
-                    onOpen={handleOpenResultLimit}
-                    value={resultLimit}
-                    onChange={handleChangeResultLimit}
-                    >
-                    <MenuItem value={10}>10</MenuItem>
-                    <MenuItem value={20}>20</MenuItem>
-                    <MenuItem value={30}>30</MenuItem>
+                        labelId="demo-controlled-open-select-label"
+                        id="demo-controlled-open-select"
+                        open={openResult}
+                        onClose={handleCloseResultLimit}
+                        onOpen={handleOpenResultLimit}
+                        value={resultLimit}
+                        onChange={handleChangeResultLimit}>
+                        <MenuItem value={10}>10</MenuItem>
+                        <MenuItem value={20}>20</MenuItem>
+                        <MenuItem value={30}>30</MenuItem>
                     </Select>
                 </FormControl>
             </div>
             <h1 className={classes.text_center}>Tapahtumat</h1>
-            
+
             {events && (
                 <>
                     {events.data.map(x => {
--- frontend/src/services/getTagNames.js
+++ frontend/src/services/getTagNames.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const getTagNames = async (tagNames) => {
+export const getTagNames = async tagNames => {
     const response = await axios.get('api/tags/')
     return response
 }
