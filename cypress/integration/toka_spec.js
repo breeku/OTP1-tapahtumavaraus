6382a90ff4ca34bceeeb53167400301ef3de252d
Samuel <samuel.ahjoniemi@gmail.com>
2020-09-18 13:58:59 +0300

    lisää testejä

--- cypress/integration/toka_spec.js
+++ cypress/integration/toka_spec.js
@@ -1,7 +1,21 @@
-const { cyan } = require("color-name")
+describe('Ensimmäiset testit', () => {
+    it('Pääsee onnistuneesti etusivulle', function () {
+      cy.visit('/')
 
-describe('My First Test', () => {
-    it('menee sivulle', function () {
-      cy.visit('http://localhost:3000/')
+      cy.contains('Koti')
+    })
+    it('Menee etusivulle ja painaa tapahtumat välilehteä', () => {
+      cy.visit('/')
+
+      cy.get('#root > div.makeStyles-root-1 > header > div > a:nth-child(2) > button > span.MuiButton-label').click()
+    })
+    it('Vaihtaa rajauksia tapahtumat sivulla', () => {
+      cy.visit('/events')
+
+     cy.get('#root > div.makeStyles-rootElement-10 > div:nth-child(2) > button').click()
+       .get('#menu- > div.MuiPaper-root.MuiMenu-paper.MuiPopover-paper.MuiPaper-elevation8.MuiPaper-rounded > ul > li:nth-child(2)').click()
+
+       .get('#resultLimit > span.MuiButton-label').click()
+       .get('#menu- > div.MuiPaper-root.MuiMenu-paper.MuiPopover-paper.MuiPaper-elevation8.MuiPaper-rounded > ul > li:nth-child(2)').click()
     })
   })
