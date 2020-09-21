2bb1ce90ee60989ee5942cb09e1a59015520b318
Samuel <samuel.ahjoniemi@gmail.com>
2020-09-21 14:45:28 +0300

    testit toimii

--- cypress/integration/toka_spec.js
+++ cypress/integration/toka_spec.js
@@ -12,25 +12,25 @@ describe('Ensimmäiset testit', () => {
     it('Vaihtaa rajauksia tapahtumat sivulla', () => {
       cy.visit('/events')
 
-     cy.get('#root > div.makeStyles-rootElement-10 > div:nth-child(2) > button').click()
-       .get('#menu- > div.MuiPaper-root.MuiMenu-paper.MuiPopover-paper.MuiPaper-elevation8.MuiPaper-rounded > ul > li:nth-child(2)').click()
-
-       .get('#resultLimit > span.MuiButton-label').click()
-       .get('#menu- > div.MuiPaper-root.MuiMenu-paper.MuiPopover-paper.MuiPaper-elevation8.MuiPaper-rounded > ul > li:nth-child(2)').click()
-    })
+      cy.get('#root > div.makeStyles-rootElement-11 > div > div:nth-child(1) > button').click()
+       .get('#menu- > div.MuiPaper-root-326.MuiMenu-paper-322.MuiPopover-paper-325.MuiPaper-elevation8-337.MuiPaper-rounded-327 > ul > li:nth-child(1)').click()
+      
+       
+       .get('#resultLimit').click()
+       .get('#menu- > div.MuiPaper-root-618.MuiMenu-paper-614.MuiPopover-paper-617.MuiPaper-elevation8-629.MuiPaper-rounded-619 > ul > li:nth-child(2)').click()
+    
+       .get('#resultLimit').click()
+       .get('#menu- > div.MuiPaper-root-910.MuiMenu-paper-906.MuiPopover-paper-909.MuiPaper-elevation8-921.MuiPaper-rounded-911 > ul > li:nth-child(1)').click()
+      })
     it('Hakusivulle saadaan tapahtuma ja löydetään se', () => {
       cy.visit('/events')
 
-      cy.get('#root > div.makeStyles-rootElement-10 > div:nth-child(1) > button > span.MuiButton-label').click()
-        .get('#menu- > div.MuiPaper-root.MuiMenu-paper.MuiPopover-paper.MuiPaper-elevation8.MuiPaper-rounded > ul > li:nth-child(1)').click()
+      cy.get('#root > div.makeStyles-rootElement-11 > div > div:nth-child(1) > button').click()
+        .get('#menu- > div.MuiPaper-root-326.MuiMenu-paper-322.MuiPopover-paper-325.MuiPaper-elevation8-337.MuiPaper-rounded-327 > ul > li:nth-child(1)').click()
+      
+        .get('#root > div.makeStyles-rootElement-11 > div > div:nth-child(3) > button > span.MuiButton-label-370').click()
+        .get('#menu- > div.MuiPaper-root-618.MuiMenu-paper-614.MuiPopover-paper-617.MuiPaper-elevation8-629.MuiPaper-rounded-619 > ul > li:nth-child(2)').click()
 
-        .get('#root > div.makeStyles-rootElement-10 > div:nth-child(2) > button').click()
-        .get('#menu- > div.MuiPaper-root.MuiMenu-paper.MuiPopover-paper.MuiPaper-elevation8.MuiPaper-rounded > ul > li:nth-child(2)').click()
-
-        .get('#resultLimit > span.MuiButton-label').click()
-        .get('#menu- > div.MuiPaper-root.MuiMenu-paper.MuiPopover-paper.MuiPaper-elevation8.MuiPaper-rounded > ul > li:nth-child(2)').click()
-        .wait(1)
-
-        .get('#root > div.makeStyles-rootElement-10 > a:nth-child(5) > div > div')
+        .get('#root > div.makeStyles-rootElement-11 > a:nth-child(3) > div > div > p')
       })
   })
