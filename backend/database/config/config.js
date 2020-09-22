40b5aae95c9827070a4026299d34008bb633949f
breeku <bregfi@hotmail.com>
2020-09-22 16:42:11 +0300

    conf

--- backend/database/config/config.js
+++ backend/database/config/config.js
@@ -3,8 +3,9 @@ module.exports = {
     development: {
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
         database: 'database_development',
-        host: 'localhost',
         dialect: 'postgres',
     },
     test: {
