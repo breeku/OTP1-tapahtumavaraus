a25a785ec6439e1043ca39f25fd4e047a83f7bef
Santeri Einola <einola.santeri@gmail.com>
2020-09-24 14:07:18 +0300

    Kirjautumis- ja käyttäjätunnuksen luontisivut luotu

--- frontend/src/components/Login/index.jsx
+++ frontend/src/components/Login/index.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { makeStyles } from '@material-ui/core/styles'
+import SignIn from './components/signIn'
+import SignUp from './components/signUp'
+import Link from '@material-ui/core/Link'
+import Grid from '@material-ui/core/Grid'
 
 const useStyles = makeStyles(theme => ({
     text_center: theme.text_center,
 }))
 
-export default function Login() {
+export default function Home() {
     const classes = useStyles()
+    const [signInElement, setSignInElement] = useState(true)
+    const [signUpElement, setSignUpElement] = useState(false)
+
     return (
         <div>
-            <h1 className={classes.text_center}>Kirjaudu</h1>
+            {signInElement && !signUpElement && (
+                <>
+                    <SignIn />
+                    <Grid item>
+                        <Link href="#" variant="body2">
+                            {'Eikö sinulla ole käyttäjätunnusta? Luo käyttäjätunnus'}
+                        </Link>
+                    </Grid>
+                </>
+            )}
+            ,{!signInElement && signUpElement && <SignUp />}
         </div>
     )
 }
