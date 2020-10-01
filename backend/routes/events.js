4fa04917760317bf794a1862375faa356505dda5
breeku <bregfi@hotmail.com>
2020-10-01 16:10:02 +0300

    korjaus importtiin

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const axios = require('axios')
-const BASEURL = require('../config/index')
+const { BASEURL } = require('../config/index')
 const db = require('../database/models')
 
 const eventsRouter = express.Router()
