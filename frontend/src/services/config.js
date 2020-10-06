e588bc17f01291b255d09ad61642f34aae2c5461
Samuel <samuel.ahjoniemi@gmail.com>
2020-10-06 15:46:13 +0300

    https lisätty

--- frontend/src/services/config.js
+++ frontend/src/services/config.js
@@ -1,2 +1,2 @@
 export const BASEURL =
-    process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : 'api.tapahtumavaraus.tk'
+    process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : 'https://api.tapahtumavaraus.tk'
